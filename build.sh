3132aaf4d71c4710d56d30b510cc6f22da44c151
Robert <rkalescky@smu.edu>
2022-10-17 17:17:57 -0500

    Added module file.

--- build.sh
+++ build.sh
@@ -9,5 +9,14 @@ docker build -t data_arts:${version} .
 # Convert Docker image to Singularity image
 docker run -v /var/run/docker.sock:/var/run/docker.sock\
  -v $PWD:/output --privileged -t --rm\
- singularityware/docker2singularity data_arts:${version}
+ singularityware/docker2singularity data_arts:${version} | tee build.log
+
+# Get Singularity image name
+img_name=$(grep "Build complete:" build.log | tr -s ' ' | cut -d' ' -f5 | cut -d'/' -f3 | tr -d '\b\r')
+
+# Change Singularity image permissions
+sudo chown $USER:$USER $img_name
+
+# Update module file with new Singularity image name
+sed -i -e "/local img_name/c\local img_name      = \'${img_name}\'" data_arts.lua
 
---
+++ data_arts.lua
@@ -0,0 +1,19 @@
+always_load("singularity")
+
+local img_name      = 'data_arts_latest-2022-10-17-2c698b627cd6.simg'
+local img_directory = '/hpc/applications/containers/'
+local img_path      = pathJoin(img_directory, img_name)
+
+function build_command(cmd)
+  local cmd_beginning = 'singularity exec '
+  local cmd_ending    = img_path .. ' ' .. cmd
+  local sh_ending     = ' "$@"'
+  local csh_ending    = ' $*'
+  local sh_cmd        = cmd_beginning .. cmd_ending .. sh_ending
+  local csh_cmd       = cmd_beginning .. cmd_ending .. csh_ending
+  set_shell_function(cmd, sh_cmd, csh_cmd)
+end
+
+build_command('python3')
+build_command('jupyter-lab')
+
